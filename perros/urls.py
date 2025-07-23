ca720617e9b8d63411e06ab16c7f4883a0b699c3
DigitalBc498 <anarusconi3@gmail.com>
2025-07-23 20:06:47 -0300

    modificacion de settings

--- perros/urls.py
+++ perros/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('publicar/', views.publicar_perro, name='publicar'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(BASE_DIR, 'static'))
